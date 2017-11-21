02390fc1a21da4b4626af1c75a5a3de6efb0c9a1
syarotta1997 <helenlu1997@gmail.com>
2017-11-21 02:14:58 -0500

    1

--- ext2_mkdir.c
+++ ext2_mkdir.c
@@ -42,6 +42,7 @@ int ftree_visit(struct ext2_dir_entry * dir, struct path_lnk* p){
        int count = (int)dir->rec_len; 
        int size = ino[dir->inode].i_size;
        while ( count <= size ){
+           printf("%s\n",dir->name);
            if (dir->file_type == EXT2_FT_DIR){
                char name[dir->name_len+1];
                memset(name, '\0', dir->name_len+1);
