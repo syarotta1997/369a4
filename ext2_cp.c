066bd304e92773aff954e4e9063dded2295ebcb8
syarotta1997 <helenlu1997@gmail.com>
2017-11-22 22:27:04 -0500

    1

--- ext2_cp.c
+++ ext2_cp.c
@@ -40,7 +40,7 @@ int main(int argc, char **argv) {
         exit(1);
     }
     if (f_name == NULL){
-        strcpy(f_name,"/");
+        f_name = "/";
         strcpy(f_name,source_path);
     }
     if (strrchr(target_path,'/') - target_path == strlen(target_path) - 1)
