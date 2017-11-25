48b37207c31f4df92d15d8131421df73f430092c
syarotta1997 <helenlu1997@gmail.com>
2017-11-25 00:15:49 -0500

    1

--- helper.h
+++ helper.h
@@ -43,7 +43,7 @@ void destroy_list();
 int pad_path(char* source_path, char* target_path);
 void init_inode(unsigned short inode_index, unsigned short size,char type );
 int make_dir(unsigned short inum);
-int copy_file(struct stat* stats, unsigned short parent_inode,char* source_path,char* f_name);
+int copy_file(struct stat* stats, unsigned short parent_inode,char* source_path);
 void update_dir_entry(unsigned short inum, unsigned short inode_num,char* name, unsigned char type);
 int hard_link(unsigned short source_inode,unsigned short parent_inode,char* link_name);
 int sym_link(unsigned short parent_inode, char* path,char* link_name);
