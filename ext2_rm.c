5a331c71c50a6e5fe25c7f3f5ed252fbe9695140
syarotta1997 <helenlu1997@gmail.com>
2017-11-25 14:36:18 -0500

    commit

--- ext2_rm.c
+++ ext2_rm.c
@@ -18,6 +18,7 @@ extern unsigned char block_bitmap[128];
 extern unsigned char inode_bitmap[32];
 extern struct path_lnk* p;
 extern char* new_dir;
+extern char* new_name;
 extern char dir_flag;
 
 int main(int argc, char **argv) {
