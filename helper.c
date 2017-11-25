17b8688a682670ce82b20d373514938629dda681
syarotta1997 <helenlu1997@gmail.com>
2017-11-24 21:16:26 -0500

    1

--- helper.c
+++ helper.c
@@ -130,7 +130,7 @@ int chk_source_path(char* source_path, char* target_path){
     construct_path_linkedlst(new);
     int root_block = ino_table[1].i_block[0];
     struct ext2_dir_entry *dir = (struct ext2_dir_entry *)(disk + (1024* root_block));
-    return result = ftree_visit(dir, 2, p->next, "cp");
+    return ftree_visit(dir, 2, p->next, "cp");
 }
 
 int ftree_visit(struct ext2_dir_entry * dir, unsigned short p_inode ,struct path_lnk* p, char* type){
