fb5e3b40e286a90f039e5350b210309001770c2d
syarotta1997 <helenlu1997@gmail.com>
2017-11-27 19:32:40 -0500

    commit

--- helper.c
+++ helper.c
@@ -259,7 +259,7 @@ void check_all(struct ext2_dir_entry * dir){
    
     int count = (int)cur->rec_len; 
       
-    printf("============== layer [ %s ]================\n\n",dir->inode);
+    printf("============== layer [ %d ]================\n\n",dir->inode);
     while ( count <= 1024 ){
             printf(" %s -- current at inode[%d]  rec_len: %d  %s\n",dir->name,cur->inode,cur->rec_len,cur->name);        
             cur_inode = (struct ext2_inode*) (ino_table+cur->inode-1);       
